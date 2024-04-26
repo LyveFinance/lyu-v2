1bdbf1abe4f6cc5b72a9d3945edacf3bb673295e
LyveFinance <alsudayes@gmail.com>
2024-04-26 16:31:51 +0800

    update file

--- contracts/mock/CurveRouter.sol
+++ contracts/mock/CurveRouter.sol
@@ -10,11 +10,13 @@ import "../Interfaces/ICurveRouter.sol";
 contract CureRouter is ICurveRouter,Ownable{
 
   IERC20 public immutable  WETH;
+  IERC20 public immutable  lyu;
 
-  constructor( IERC20 _WETH){
+  constructor( IERC20 _WETH,IERC20 _lyu){
        WETH = _WETH;
     }
     function exchange(address[11] memory _router, uint256[5][5] memory _swap_params,uint256 _amount,uint256 _expected,address[5] memory _pools) external returns (uint256 ){
+       IERC20(lyu).transferFrom(msg.sender,address(this),_amount);
        IERC20(WETH).transfer(msg.sender,_expected);
        return _expected;
     }
